c264fe6b8cfe8678a7619c11f6a196e19999d3d8
Yun-He-Lee <yun-he-lee@Yun-He-LeedeMacBook-Pro.local>
2015-09-04 16:37:06 +0800

    first pro

---
+++ EarMaster/EarMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EarMaster
+//
+//  Created by Yun-He-Lee on 15/9/4.
+//  Copyright (c) 2015年 Yun-He-Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EarMaster/EarMaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EarMaster
+//
+//  Created by Yun-He-Lee on 15/9/4.
+//  Copyright (c) 2015年 Yun-He-Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
